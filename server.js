a901f7cdf98ba2fa2be9926da23b2868a6a3bc7f
rquiroz3290 <rquiroz3290@gmail.com>
2021-02-21 15:16:03 -0500

    adding more code for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,6 +23,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",   {
 });
 
 // routes
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
